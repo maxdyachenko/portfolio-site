b7cc18ee82e7dc697d1c42b4c0a00053cab28f82
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-01-04 12:22:34 +0200

    add all includes properly

--- htdocs/wp-content/themes/dyachenko/functions.php
+++ htdocs/wp-content/themes/dyachenko/functions.php
@@ -1,28 +1,29 @@
 <?php
 
-// Load the theme stylesheets
+add_action('wp_enqueue_scripts', 'md_styles_scripts');
 function md_styles_scripts()
 {
 
-	wp_register_style( 'owlcarousel', get_template_directory_uri() . '/css/owlcarousel/owlcarousel.min.css' );
-	wp_register_style( 'owlcarouseltheme', get_template_directory_uri() . '/css/owlcarousel/owlcarousel.theme.default.min.css' );
+	wp_register_style( 'owlcarousel', get_template_directory_uri() . '/css/owlcarousel/owl.carousel.min.css' );
+	wp_register_style( 'owlcarouseltheme', get_template_directory_uri() . '/css/owlcarousel/owl.theme.default.min.css' );
 
-	wp_register_script('main_js', get_template_directory_uri()  . '/js/main.js');
-	wp_register_script('owlcarousel_js', get_template_directory_uri()  . '/js/owl.carousel.min.js');
-	wp_register_script( 'touch_js', get_template_directory_uri() . '/js/touch.js' );
+	wp_register_script('main_js', get_template_directory_uri()  . '/js/main.js', array('jquery'), null, true);
+	wp_register_script('owlcarousel_js', get_template_directory_uri()  . '/js/owl.carousel.min.js',array('jquery'), null, true);
+	wp_register_script( 'touch_js', get_template_directory_uri() . '/js/touch.js', array('jquery'), null, true );
 
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/main.js' );
-	wp_enqueue_script( 'touch_js', get_template_directory_uri() . '/js/touch.js' );
+	wp_enqueue_script( 'main_js');
+	wp_enqueue_script( 'touch_js');
 
 	if(is_home() || is_front_page() ) {
 		wp_enqueue_style('owlcarousel');
 		wp_enqueue_style('owlcarouseltheme');
 
-		wp_enqueue_script( 'owlcarousel_js', get_template_directory_uri() . '/js/owl.carousel.min.js' );
+		wp_enqueue_script( 'owlcarousel_js');
 	}
 
 }
-add_action('wp_enqueue_scripts', 'md_styles_scripts');
+
+
 
 ?>
--- htdocs/wp-content/themes/dyachenko/js/main.js
+++ htdocs/wp-content/themes/dyachenko/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
     var menu = $('.menu'),
         isMenuOpen = false;
     $('.hamburger').click(function () {
